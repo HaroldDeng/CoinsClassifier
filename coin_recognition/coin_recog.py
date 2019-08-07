897dafe42e5a7694146d19ec1b03f0045bc3d857
William Swiss <willswiss001@gmail.com>
2019-08-07 14:40:48 -0400

    added file to separate coin recognition from the circle detection work under 'coin_recognition'

---
+++ coin_recognition/coin_recog.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import keras
+import tf
+
+# input to the CNN will be cropped images of each coin we want to recognize
+# output will be of 11 different results: heads or tail of each denomination, or unknown
+
