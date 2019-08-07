d94f35d556d3a4eccbee8cedfe35f08f9f61aae5
mailmindlin <mailmindlin@users.noreply.github.com>
2019-08-07 13:25:42 -0400

    Add hough sample script

--- circle_detection/from_img.py
+++ circle_detection/from_img.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from window import ImageWindow
-from radsym import RadSymTransform
+from circle import HoughCircleTransform
 from sys import argv
 
 # create an overlay image. You can use any image
@@ -10,11 +10,34 @@ iname = argv[1]
 
 
 # read the frame
-frame = cv2.imread(iname)
-for i in range(10, 110, 20):
-	radSym = RadSymTransform(i, minval=10)
 
-	grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	frame = radSym(grayImg)
-	frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
-	cv2.imwrite(f'out_{i}.jpg', frame)
+with ImageWindow('a') as window:
+	# tbP1 = window.trackbar('Param1', 50, 100)
+	tbP1 = 50
+	tbP2 = 25#window.trackbar('Param2', 35, 100)
+	tbIter = 2# window.trackbar('MItrs', 4, 10)
+	while window.isOpen():
+		frame = cv2.imread(iname)
+		frame = cv2.GaussianBlur(frame, (15, 15), 0)
+		print(int(tbP1), int(tbP2))
+		findCircles = HoughCircleTransform(1, 40, param1=int(tbP1), param2=int(tbP2), minRadius=5, maxRadius=50)
+		grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		grayImg = cv2.adaptiveThreshold(grayImg, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 1)
+		kernel = np.ones((3, 3), np.uint8)
+		grayImg = cv2.morphologyEx(grayImg, cv2.MORPH_CLOSE, kernel, iterations=int(tbIter))
+
+		frame = grayImg
+		# frame = cv2.Canny(frame, int(tbP1), int(tbP1) // 2)
+		frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+
+		circles = findCircles(grayImg)
+		if circles is not None:
+			circles = np.uint16(np.around(circles))
+			for x, y, rad in circles[0,:]:
+				# draw the outer circle
+				cv2.circle(frame, (x, y), rad, (0,255,0), 2)
+				# draw the center of the circle
+				cv2.circle(frame, (x, y), 2, (0,0,255), 3)
+		window.show(frame)
+		if cv2.waitKey(1) == ord('q'):
+			break
