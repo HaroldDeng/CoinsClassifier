d2b9ae2f9d2750432938504d1c3e2181dd872678
HaroldDeng <dengz5@rpi.edu>
2019-08-15 10:17:05 -0400

    Added threading

--- Driver.py
+++ Driver.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+
 from tkinter import *
+import time, threading
 
 
-""" 2 windows
+""" 1 window
     -------------    --------------
     |           |    |US conins   |
     |   video   |    |.; total|
@@ -17,10 +20,11 @@ from tkinter import *
 class GUI:
     # WIDTH = 480
     # HEIGHT = 480
+    usCoinsValue = ["1¢", "5¢", "10¢", "25¢", "50¢", "$1"]
 
     def __init__(self):
         self.input = [0, 0, 0, 0]
-        self.labelList = [0, 0, 0, 0, 0, 0, 0]  # 
+        self.labelList = []
 
         self.root = Tk()
         self.root.title("Image Window")
@@ -32,6 +36,7 @@ class GUI:
 
         self._inflateImg()
         self._inflateRes()
+        threading.Thread(target=self._infiniteUpdate, args=()).start()
         self.imgFrame.mainloop()
 
     def _inflateImg(self):
@@ -58,7 +63,7 @@ class GUI:
         acc.grid(row=0, column=1)
 
         Label(f, text="minRad", font="Helvetica, 15").grid(row=1, column=0)
-        minRad= Scale(f, from_=0, to=100, length=300,
+        minRad = Scale(f, from_=0, to=100, length=300,
                        orient=HORIZONTAL, command=listener)
         minRad.grid(row=1, column=1)
 
@@ -73,8 +78,7 @@ class GUI:
         dp.grid(row=3, column=1)
 
     def _inflateRes(self):
-        usCoinsValue = ["1¢", "5¢", "10¢", "25¢", "50¢", "$1"]
-        self._inflateUS(usCoinsValue)
+        self._inflateUS(self.usCoinsValue)
 
     def _inflateUS(self, value: [str]):
         x, y = 0, 0
@@ -83,9 +87,12 @@ class GUI:
             Label(self.resFrame, text=val, font="Helvetica 16").grid(
                 row=y, column=x)
             x += 1
-            #.labelList
-            Label(self.resFrame, text=str(x)+str(y), font="Helvetica 20 bold", padx=20, anchor=W).grid(
-                row=y, column=x)
+
+            # labe
+            lb = Label(self.resFrame, text=str(x)+str(y),
+                       font="Helvetica 20 bold", padx=20, anchor=W)
+            lb.grid(row=y, column=x)
+            self.labelList.append(lb)
 
             if x == 3:
                 x = 0
@@ -98,8 +105,22 @@ class GUI:
         Label(self.resFrame, text="Total",
               font="Helvetica 30 bold").grid(row=y, column=x, columnspan=2)
         x = 2
-        Label(self.resFrame, text="0.00",
-              font="Helvetica 30 bold").grid(row=y, column=x, columnspan=2)
+        lb = Label(self.resFrame, text="0.00", font="Helvetica 30 bold")
+        lb.grid(row=y, column=x, columnspan=2)
+        self.labelList.append(lb)
+
+    def _infiniteUpdate(self):
+        num = 1
+        while True:
+            for i in range(len(self.labelList)):
+                if i < 6:
+                    self.labelList[i].configure(text = str(num))
+                else:
+                    # ["1¢", "5¢", "10¢", "25¢", "50¢", "$1"]
+                    total = num * (0.01 + 0.05 + 0.1 + 0.25 + 0.5 + 1)
+                    self.labelList[i].configure(text = "{:10.2f}".format(total))
+            num += 1
+            time.sleep(0.5) # sleep for 0.2 second
 
 
 if __name__ == "__main__":
