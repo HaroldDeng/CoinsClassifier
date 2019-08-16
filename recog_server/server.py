39c0c6254d8a69a43a7e70113c581871fc04b69c
William Swiss <willswiss001@gmail.com>
2019-08-16 14:02:24 -0400

    blah

--- recog_server/server.py
+++ recog_server/server.py
@@ -1,7 +1,7 @@
 import argparse
 from flask import Flask, request, send_from_directory
 from circle_in import crop_circles
-from test_web import main as tw
+import test_web as tw
 import cv2
 import numpy as np
 import shutil
@@ -41,17 +41,18 @@ def analyze_image():
 	frames = []
 	try:
 		os.mkdir('./tmp')
+		os.mkdir('./tmp/tmp')
 	except OSError:
 		pass
 	
 	def write_callback(frame, i, x, y, r, **kwargs):
-		result.append({'x': x, 'y': y, 'width': r, 'height': r, })
+		#result.append({'x': x, 'y': y, 'width': r, 'height': r, })
 		fname = dst.format_map({'src': src, 'i': i, 'x': x, 'y': y, 'r': r, 'radius': r})
-		cv2.imwrite(frame, f'./tmp/gen_{}.jpg'.format(i))
+		cv2.imwrite(frame, './tmp/tmp/gen_{}.jpg'.format(i))
 	crop_circles(image, write_callback)
 	cents = tw.main('./tmp')
 	shutil.rmtree('./tmp', ignore_errors=True)
-	return f'{"value":{cents}}', 200, { 'Content-type': 'application/json' }
+	return '{"value":{}}'.format(cents), 200, { 'Content-type': 'application/json' }
 
 
 if __name__ == "__main__":
