99f4908dacc29fffa40bdff78a690f3add65d7c3
mailmindlin <mailmindlin@users.noreply.github.com>
2019-08-16 13:08:00 -0400

    Server file upload

--- recog_server/server.py
+++ recog_server/server.py
@@ -1,6 +1,8 @@
 import argparse
 from flask import Flask, request, send_from_directory
-
+from circle_in import crop_circles
+import cv2
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', default=8080, help='port to start server on', type=int)
@@ -24,13 +26,20 @@ def serve_js(path):
 def serve_image(path):
 	return send_from_directory(args.images, path)
 
-
 @app.route('/api/analysis', methods=['GET','POST'])
 def analyze_image():
 	print('0')
 	blob = request.files['image']
 	print('a')
-	print(blob)
+	arr = np.array(blob.read())
+	image = cv2.imdecode(arr, cv2.CV_LOAD_IMAGE_COLOR)
+
+	frames = []
+	def write_callback(frame, i, x, y, r, **kwargs):
+		result.append({'x': x, 'y': y, 'width': r, 'height': r, })
+		fname = dst.format_map({'src': src, 'i': i, 'x': x, 'y': y, 'r': r, 'radius': r})
+	crop_circles(image, write_callback)
+
 	return '[]', 200, { 'Content-type': 'application/json' }
 
 
